0eb71ff1adcee3b6ec5e0647ee761e09ac4df49c
Eric Crooks <eric@armorpayments.com>
2017-10-18 16:36:57 -0400

    Add ExampleApp_NetFramework461_NetStandard16
    This example app uses .NET Framework 4.6.1 to check if .NET Standard 1.6 compatibility works (compatibility with .NET Standard 1.4, 1.5, 1.6, and 2.0, are also included in this since it's using .NET Framework 4.6.1)

---
+++ ExampleApp_NetFramework461_NetStandard16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp_NetFramework461_NetStandard16
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Get Payoneer Escrow client to make requests
+			PayoneerEscrow.Api.Client client = new PayoneerEscrow.Api.Client(
+				"your_api_key",
+				"your_api_secret",
+				true);
+
+			try
+			{
+				// Get all clients associated with the API key
+				Console.WriteLine("\n\nGetting all clients...\n\n");
+				string accounts = client.Accounts().All();
+				Console.WriteLine(accounts);
+
+
+				Console.WriteLine("\n\nGetting a single account...\n\n");
+				string account = client.Accounts().Users("170246583945").All();
+				Console.WriteLine(account);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			// Keep program open
+			Console.Write("\n\nPress [Enter] to quit.");
+			Console.ReadLine();
+		}
+	}
+}
