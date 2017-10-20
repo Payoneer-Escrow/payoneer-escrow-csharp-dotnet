29e941d713a26a23f2e960de6f1bea53ee9a9e6b
Eric Crooks <eric@armorpayments.com>
2017-10-20 15:57:04 -0400

    Add ExampleApp_NetCore10_NetStandard12-20
    * The target framework is netcoreapp1.0
    * The program was built using Visual Studio 2017 on macOS
    * It uses .NET Core 1.0.5 (has been installed locally on macOS and tested)
    * Checks compatibility with .NET Standard 1.2 to 2.0

---
+++ ExampleApp_NetCore10_NetStandard12-20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExampleApp_NetCore10_NetStandard12_20
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Get Payoneer Escrow client to make requests
+			PayoneerEscrow.Api.Client client = new PayoneerEscrow.Api.Client(
+				"your_api_key",
+				"your_api_secret",
+				true);
+
+			try
+			{
+				// Get all clients associated with the API key
+				Console.WriteLine("\n\nGetting all clients...\n\n");
+				string accounts = client.Accounts().All();
+				Console.WriteLine(accounts);
+
+
+				Console.WriteLine("\n\nGetting a single account...\n\n");
+				string account = client.Accounts().Get("170246583945");
+				Console.WriteLine(account);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			// Keep program open
+			Console.Write("\n\nPress [Enter] to quit.");
+			Console.ReadLine();
+		}
+	}
+}
